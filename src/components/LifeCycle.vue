902b58c8a5c12d345c175c4024c053abb2ec6f38
eazybright <doneazy911@gmail.com>
2020-09-17 15:07:29 +0100

    lifecycle hook

--- src/components/LifeCycle.vue
+++ src/components/LifeCycle.vue
@@ -5,8 +5,21 @@
             </li>
         </ul> -->
 
-         <div class="about">
-            <h1>Hello {{ name }}, this is an lifecycle page</h1>
+        <div class="about">
+            <h1>Hello {{ fullName }}, this is an lifecycle page</h1>
+            <h1>Reversed Name: {{ reversedName }}</h1>
+        </div>
+
+        <div>
+            <input type="number" v-model="id" />
+            <p>Name: {{user.name}}</p>
+            <p>Email: {{user.email}}</p>
+            <p>Id: {{user.id}}</p>
+        </div>
+        <div>
+            <h1>{{msg}}</h1>
+            <button @click="msg = 'Updated Hook'">Update Message</button>
+            <button @click="remove">Destroy instance</button>
         </div>
     </div>   
 </template>
@@ -18,25 +31,62 @@
 
 // New Observable stream of string array values
 // const people$ = Observable.of(['Paul', 'Katie', 'Bob']);
+import axios from 'axios';
 
-export default  {
+export default{
     name: 'LifeCycle',
     data() {
         return {
-            name: 'Paul'
+            fullName: 'Paul',
+            id: '',
+            user: {},
+            msg: "lifecycle hook"
         }
     },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
     created() {
-        console.log(`Created: Hello ${this.name}`)
+        console.log(`Created: Hello ${this.fullName}`)
     },
-
+    beforeMount() {
+        console.log('beforeMount')
+    },
     mounted() {
-        console.log(`Mounted: Hello ${this.name}`)
+        console.log(`Mounted: Hello ${this.fullName}`)
         
     },
-    // methods: {
-        
-    // },
+    beforeUpdated() {
+        console.log('beforeUpdated') 
+    },
+    updated() {
+        console.log('updated')
+    },
+    computed: {
+        reversedName(){
+            return this.fullName.split('').reverse().join('') 
+        }
+    },
+    methods: {
+        getDataForUser() { 
+            axios.get(`https://jsonplaceholder.typicode.com/users/${this.id}`)
+            .then(res => this.user = res.data);
+        },
+        remove(){
+            this.$destroy()
+        }
+    },
+    watch: {
+        id() {
+            this.getDataForUser()
+        }
+    },
+    beforeDestroy(){
+        console.log("Before destroy")
+    },
+    destroyed(){
+        console.log("Destroyed")
+    }
     // subscriptions() {
     //    const people$ = Observable.of(['Paul', 'Katie', 'Bob'])
     //     return {
