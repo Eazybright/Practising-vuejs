85ee009e6ce22bc5b40a19472a6463a28178ae17
eazybright <doneazy911@gmail.com>
2020-09-29 15:06:02 +0100

    using multiple modules in vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,38 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const moduleA = {
+  state: () => ({
+    count: 0
+  }),
+  mutations: {
+    increment(state){
+      state.count++
+    }
+  },
+  actions: {
+    incrementIfOddOnRootSum ({ state, commit, rootState }) {
+     if ((state.count + rootState.count) % 2 === 1) {
+       commit('increment')
+     }
+   }
+  },
+  getters: {
+    doubleCount (state) {
+      return state.count * 2
+    },
+    sumWithRootCount (state, getters, rootState) {
+      return state.count + rootState.count
+    }
+  }
+}
+
+const moduleB = {
+  state: () => ({  }),
+  mutations: {  },
+  actions: {  }
+}
+
 const store = new Vuex.Store({
   state: {
     count: 0
@@ -44,6 +76,10 @@ const store = new Vuex.Store({
      })
      console.log(this.state.count)
    }
+ },
+ modules: {
+   a: moduleA,
+   b: moduleB
  }
 })
 
